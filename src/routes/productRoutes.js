489703eb54cefbd5c83a8e47ee6c01f17a1e497f
betacourtlsofie <jeanortiz00@gmail.com>
2021-12-02 12:01:13 -0500

    first commit

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController')
+
+router.post('/',productController.addProduct);
+router.post('/list',productController.getAllProduct)
+router.get('/:id',productController.getProducts)
+router.post('/offert',productController.addOffert)
+router.get('/offert/:id',productController.getOffert)
+router.get('/offert/all/:id',productController.getAllOfferts)
+router.post('/offert/accept', productController.acceptOffert)
+router.post('/offert/refuse', productController.refuseOffert)
+
+module.exports = router;
